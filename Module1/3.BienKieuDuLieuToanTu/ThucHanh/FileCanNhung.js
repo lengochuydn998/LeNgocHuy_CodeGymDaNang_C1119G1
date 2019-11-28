09fdaf3d9dc58d114d7562bbd080fa292780df27
lengochuydn <lengochuydn998@gmail.com>
2019-11-28 15:54:14 +0700

    ngay28

---
+++ Module1/3.BienKieuDuLieuToanTu/ThucHanh/FileCanNhung.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+let area;
+    inputHeight = prompt("InputHeight");
+    inputWidth  = prompt("InputWidth");
+        let width = parseInt(inputWidth);
+        let height = parseInt(inputHeight);
+        area = width * height;
+document.write("The area is: " + area);
+
+
+
